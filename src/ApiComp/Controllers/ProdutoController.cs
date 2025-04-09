63033c60abcab95775ff4d80a7bd8de779ba86b4
Josiel <cjosiel2@gmail.com>
2025-04-09 18:00:10 -0300

    InciadoProdutoController

---
+++ src/ApiComp/Controllers/ProdutoController.cs
@@ -0,0 +1,53 @@
+﻿using ApiComp.ViewModels;
+using AutoMapper;
+using DevIO.Business.Intefaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiComp.Controllers
+{
+    [Route("api/produto")]
+    [ApiController]
+    public class ProdutoController : MainController
+    {
+        #region dependecy
+        private readonly IProdutoRepository _produtoRepository;
+        private readonly IProdutoService _produtoService;
+        private readonly IMapper _mapper;
+        private readonly INotificador _notificador;
+        #endregion
+
+
+        #region cto
+        public ProdutoController(IProdutoRepository produtoRepository, 
+                                 IProdutoService produtoService, 
+                                 IMapper mapper, 
+                                 INotificador notificador) : base(notificador)
+        {
+            _produtoRepository = produtoRepository;
+            _produtoService = produtoService;
+            _mapper = mapper;
+            _notificador = notificador;
+        }
+        #endregion
+
+
+
+        #region actions
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProdutoViewModel>>> ObterTodos()
+        {
+            var _produtoView =  _mapper.Map<IEnumerable<ProdutoViewModel>>(await _produtoRepository.ObterTodos());
+            if(!_produtoView.Any()) return NotFound("Lista vazia");
+            
+            return Ok(CustomResponse(_produtoView));
+        }
+        #endregion
+
+
+
+
+
+
+    }
+}
